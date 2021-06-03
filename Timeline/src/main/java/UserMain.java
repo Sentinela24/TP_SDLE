cfba706129300d1d47c9c76f6cf581736be77341
cvale21 <57766690+cvale21@users.noreply.github.com>
2021-06-03 20:52:18 +0100

    commit

---
+++ Timeline/src/main/java/UserMain.java
@@ -0,0 +1,12 @@
+import spread.SpreadException;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class UserMain {
+    public static void main(String args[]) throws IOException, SpreadException, ExecutionException, InterruptedException {
+        User user = new User(args[0]);
+
+        user.menu();
+    }
+}
